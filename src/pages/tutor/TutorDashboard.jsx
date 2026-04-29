81fdd15268a92ee75ce14b58e4660bc050bc7961
Nancy-1311 <nancykhundia44@gmail.com>
2026-04-29 22:37:55 +0530

    Update TutorDashboard.jsx

--- src/pages/tutor/TutorDashboard.jsx
+++ src/pages/tutor/TutorDashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
+import TutorDashboard from "./pages/tutor/TutorDashboard";
 const TutorDashboard = () => {
   const [tutor, setTutor] = useState(null);
   const [dashboard, setDashboard] = useState(null);
